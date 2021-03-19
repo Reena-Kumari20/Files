cef56e6d66f9f56ee86424f048eb0deac6175c05
Reena Kumari <reena20@navgurukul.org>
2021-03-19 22:48:44 +0530

    first commit

---
+++ question3.py
@@ -0,0 +1,11 @@
+f=open("delhi.txt","r")
+a=f.read()
+print("these all bilong to delhi")
+print(a)
+f.close()
+
+f=open("shimla.txt","r")
+b=f.read()
+print("\nthese all bilong to shimla")
+print(b)
+f.close
